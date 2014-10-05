cf95e6ac66467a233ed6de777f4dade8444c28f0
Michael Brenan <blacksmithgu@gmail.com>
2014-10-05 15:52:07 -0500

    Added handlebars views/ and kayout/ folders and an example in node.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,6 @@ module.exports = function(app) {
 	require('./api/pizzas.js')(app);
 
 	app.get('/', function(req, res) {
-      res.send('Hello, world!');
+      res.render('home', {'name' : 'Samuel Jones'});
     });
-
-    app.get('/*', function(req, res) {
-      res.send('Hello, world!');
-    });
-
 }
